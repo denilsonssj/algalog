4d44d9528e872c0c355fda6f1d4e52e77ce8c8ff
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-02 19:36:36 -0300

    Replace spring fox swagger documentation to springdoc documentation

--- src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,9 +3,6 @@ package com.algaworks.algalog.api.exceptionhandler;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.algaworks.algalog.domain.exception.DomainException;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
@@ -17,11 +14,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import lombok.AllArgsConstructor;
-import springfox.documentation.annotations.ApiIgnore;
 
-@ApiIgnore
+import com.algaworks.algalog.domain.exception.DomainException;
+
 @AllArgsConstructor
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
